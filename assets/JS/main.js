27a6789c72158e3324485dd01962e37b47369aa7
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-09-29 21:17:00 -0300

    Integração do projeto com a API de encurtar URLs

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('urlOriginal');
 const botao = document.getElementById('encurtador__botao');
 const mensagemErro = document.getElementById('mensagem__erro');
+const urlBase = 'https://api.shrtco.de/v2/shorten?url=';
 
 botao.addEventListener('click', validaInput)
 
@@ -13,3 +14,23 @@ function validaInput () {
         mensagemErro.classList.remove('mensagem__erro-ativo');
     }
 }
+
+
+const encurtaURL = async () => {
+    try{
+        var urlLonga = input.value;
+        const data = await fetch(urlBase + urlLonga);
+        const json = await data.json();
+        console.log(json);
+        return json;
+    }catch(e){
+        console.log(e.message);
+    }
+}
+
+ const buscaUrl = async () => {
+    const resp = await encurtaURL();
+    const urlCurta = resp.result.short_link
+    return urlCurta;
+ }
+ 
